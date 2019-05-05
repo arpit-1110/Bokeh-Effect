65e11cd4b2f64c528090ba14fe79aee975f33d2f
yashkhem1 <yashkhem1@gmail.com>
2019-05-06 00:51:21 +0530

    updated empty_results.sh

--- empty_results.sh
+++ empty_results.sh
@@ -1,4 +1,6 @@
-rm -rf ./results/inputs
-rm -rf ./results/pred_masks
-mkdir results/pred_masks
-mkdir results/inputs
+rm -rf cgresults/inputs/*
+rm -rf cgresults/pred_masks/*
+rm -rf cgresults/r-9-inputs/*
+rm -rf cgresults/r-9-pred_masks/*
+rm -rf p2presults/inputs/*
+rm -rf p2presults/pred_masks/*
